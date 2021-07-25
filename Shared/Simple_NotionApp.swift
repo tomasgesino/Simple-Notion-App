7207424b312deed15cf1a3468c0ced7370b901f5
Tomas Gesino <gesino.tomas@gmail.com>
2021-07-25 12:15:30 -0300

    Initial Commit

---
+++ Shared/Simple_NotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_NotionApp.swift
+//  Shared
+//
+//  Created by Tomas Gesino on 25/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Simple_NotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
